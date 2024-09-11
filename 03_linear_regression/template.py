063837c1c8bc50619a730c3cfbc8b8dd6f13e717
schmidgall <sara.schmidgall@googlemail.com>
2024-09-11 21:05:04 +0200

    Computer Ex 3 -- Done

--- 03_linear_regression/template.py
+++ 03_linear_regression/template.py
@@ -35,14 +35,18 @@ def mvn_basis(
     for j in range(features.shape[0]):
         saver = []
         for i in range(mu.shape[0]):
-            solution2 = multivariate_normal.pdf(features[j], mu[i], sigma)
+            solution2 = multivariate_normal.pdf(features[j], mu[i], var)
             saver.append(solution2)
         solution.append(saver)
     solution = torch.tensor(solution)
     return solution
 
-def _plot_mvn():
-    pass
+def _plot_mvn(fi):
+    plt.clf
+    for i in range(fi.shape[1]):
+        plt.plot(fi[:,i], label='basic fcn'+str(i))
+    plt.legend(loc='upper left')
+    plt.show()
 
 
 def max_likelihood_linreg(
@@ -60,7 +64,13 @@ def max_likelihood_linreg(
 
     Output: [Mx1], the maximum likelihood estimate of w for the linear model
     '''
-    pass
+    targets = targets.to(torch.float64) 
+    identity = torch.eye(fi.shape[1])
+    multiplicand_1 = torch.inverse((lamda * identity) + torch.matmul(torch.transpose(fi, 0, 1) , fi))
+    multiplicand_2 = torch.matmul(multiplicand_1, torch.transpose(fi, 0, 1))
+    multiplicand_2 = torch.matmul(multiplicand_2, targets)
+    multiplicand_2 = multiplicand_2.to(torch.float32)
+    return multiplicand_2
 
 
 def linear_model(
@@ -81,10 +91,17 @@ def linear_model(
 
     Output: [Nx1] The prediction for each data vector in features
     '''
-    pass
+    w = w.to(torch.float64) 
+    fi = mvn_basis(features, mu, var)
+    w = torch.t(w)
+    fi = fi.transpose(0,1)
+    y = torch.matmul(w, fi)
+    y = y.to(torch.float32)
+    return y
 
 
 if __name__ == "__main__":
+    # Section 1
     X, t = load_regression_iris()
     N, D = X.shape
     M, var = 10, 10
@@ -93,18 +110,46 @@ if __name__ == "__main__":
         mmin = torch.min(X[:, i])
         mmax = torch.max(X[:, i])
         mu[:, i] = torch.linspace(mmin, mmax, M)
+
     fi = mvn_basis(X, mu, var)
-    print(fi)
-    print(fi.shape[1])
-    print(fi.shape[0])
-    print(fi[1,2])
 
-    fig = plt.figure()
-    for i in range(fi.shape[0]):
-        for j in range(fi.shape[1]):
-            # fig = plt(fi[1, j], fi[i,0])
-            fig = plt(fi[i], fi[j])
+    # Section 2
+    #_plot_mvn(fi)
+
+    # Section 3
+    lamda = 0.001
+    wml = max_likelihood_linreg(fi, t, lamda)
+
+    # Section 4
+    prediction = linear_model(X, mu, var, wml)
+
+    # Section 5
+    plt.clf()
+    plt.plot(t, label='target')
+    plt.plot(prediction, label='prediction')
+    plt.legend(loc='upper left')
     plt.show()
-    _plot_mvn()
 
+    difference = torch.abs(t - prediction)
+    plt.clf()
+    plt.plot(difference)
+    plt.title('absolut diff = |target - prediction|')
+    plt.show()
+
+    # mean-square-error
+    # 1/n * sum((t-pred)^2)
+    sum = 0
+    for i in range(t.shape[0]):
+        sum = sum + torch.square(t[i] - prediction[i])
+    meanSquare = 1/t.shape[0] * sum
+    print(meanSquare)
+
+    # 1/2 * (t-pred)^2
+    mse = 1/2  * torch.square(t - prediction)
+    plt.clf()
+    plt.plot(mse)
+    plt.title('mean square error')
+    plt.show()
+
+    
     pass
